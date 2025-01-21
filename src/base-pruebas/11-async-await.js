aa94eb2eac4a56b96675e064339961def55b2464
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-21 15:32:38 -0300

    Pruebas con async-await

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -1,32 +1,21 @@
 
 
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+        const apiKey = 'BDVt2kd6k955Na46ECE55hTOS4QSvuXD';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
+        return url;
 
     } catch (error) {
         // manejo del error
         console.error(error)
+        return 'No se encontro la imagen';
     }
-    
-    
-    
+
 }
-
- getImagen();
-
-
-
