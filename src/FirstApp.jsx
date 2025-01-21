b19c726fb14154477457ebc418489d906f562c54
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-21 10:19:43 -0300

    Comunicacion entre componentes - Props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,11 +1,13 @@
-export const FristApp = () => {
+export const FristApp = ({title, subTitle}) => {
+
+    // console.log(props);
 
     return (
         <>
-            <h1>Jespejo</h1>
+            <h1>{title}</h1>
             {/* Expresion no objeto */}
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <p>Soy un subtitulo </p>
+            <p>{subTitle + 1}</p>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FristApp />
+        <FristApp title="Hola, Jespejo" />
     </React.StrictMode>
 )
