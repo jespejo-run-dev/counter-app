10697132151c95ee1845702e1ae0e3b9bd121c6f
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-21 10:09:27 -0300

    Impresion de variables en el HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,19 @@
+const getResult = (a,b) => {
+    return a+b;
+ }
+
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Jespejo'
+};
+
 export const FristApp = () => {
+
     return (
         <>
-            <h1>Jespejo</h1>
+            <h1>{getResult(1,10)}</h1>
+            {/* Expresion no objeto */}
+            {/* <code>{JSON.stringify(newMessage)}</code> */}
             <p>Soy un subtitulo </p>
         </>
     )
