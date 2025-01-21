db712e2f771e4404ba24fbadb7015e1d0d0c54ec
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-21 10:12:51 -0300

    Colocar estilos CSS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,17 +1,8 @@
-const getResult = (a,b) => {
-    return a+b;
- }
-
-const newMessage = {
-    message: 'Hola Mundo',
-    title: 'Jespejo'
-};
-
 export const FristApp = () => {
 
     return (
         <>
-            <h1>{getResult(1,10)}</h1>
+            <h1>Jespejo</h1>
             {/* Expresion no objeto */}
             {/* <code>{JSON.stringify(newMessage)}</code> */}
             <p>Soy un subtitulo </p>
