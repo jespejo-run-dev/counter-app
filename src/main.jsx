595e147f0b41a80f9d7d76a822a131169aab7fbc
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-21 10:38:11 -0300

    Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from'react-dom/client'
-import { FristApp } from './FirstApp'
+import { CounterApp } from './CounterApp'
 import './styles.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FristApp />
+        <CounterApp value={123} />
     </React.StrictMode>
 )
