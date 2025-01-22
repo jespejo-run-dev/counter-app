f9f4d587e1b661480eee89f2ddfcda481af53717
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-22 11:33:59 -0300

    Pruebas basicas en CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from'react-dom/client'
-import { FristApp } from './FirstApp'
 import { CounterApp } from './CounterApp'
+import { FristApp } from './FirstApp'
 import './styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FristApp title='Hola, Jespejo' />
-        {/* <CounterApp value={5} /> */}
+        {/* <FristApp title='Hola, Jespejo' /> */}
+        <CounterApp value={5} />
     </React.StrictMode>
 )
